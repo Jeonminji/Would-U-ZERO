532dcf9dcb3f77790d9c8358e20a2e03bccedf5d
hyunjungjeon <70558461+hyunjungjeon@users.noreply.github.com>
2021-05-01 22:45:00 +0900

    Update pleatsmama_scrapping.py

--- python/pleatsmama_scrapping.py
+++ python/pleatsmama_scrapping.py
@@ -3,7 +3,7 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-url = "https://pleatsmama.com/shop-all?page="
+url = "https://pleatsmama.com/shop-knitpleatsbag-all?page="
 
 #csv파일저장
 filename = "apparel2.csv"
@@ -13,14 +13,15 @@ writer = csv.writer(f)
 title = "name,price,img,link".split(",")
 writer.writerow(title)
 
+#페이지 스크래핑
 for page in range(1,8):
-    res = requests.get(url)
+    res = requests.get(url+str(page))
     soup = BeautifulSoup(res.text,"lxml")
 
     items = soup.find_all("div", attrs = {"class":"item-wrap"})
 
     for item in items:
-        name = item.find("h2").get_text() #가방 이름
+        name = item.find("h2").get_text() #제품 이름
         price = item.find("p", attrs = {"class":"pay no-margin"}).get_text() #가격
         img = item.find("img",attrs ={"class":"_org_img org_img _lazy_img"})['src'] #이미지
         link_href = item.find("a", attrs={"class":"_fade_link"})["href"] #판매 링크
