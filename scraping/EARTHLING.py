ce9f990864fd2f2add0a14cc0e75edaf7dd5b0f5
jlee99 <58466461+ammumal@users.noreply.github.com>
2021-06-18 00:28:57 +0900

    EARTHLING.py upload(미완)

---
+++ scraping/EARTHLING.py
@@ -0,0 +1,33 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+# csv생성
+filename = "Earthling.csv"
+
+f = open(filename, "w",encoding="utf-8-sig",newline="")
+writer = csv.writer(f)
+
+title = "main_category,sub_category,siteName,name,price,img,link".split(",")
+writer.writerow(title)
+
+url = "https://smartstore.naver.com/horanmalko/category/97b8cfa4060d4eec980533f5324e468e?cp=1"
+response = requests.get(url)
+bs = BeautifulSoup(response.text, 'html.parser')
+items = bs.find_all("li", attrs = {"class":"qHwcFXhj0"})
+
+# 카테고리가 없음
+def filecsv(items):
+    for item in items:
+        name = item.find("strong", attrs = {"class":"QNNliuiAk3"}).get_text() #상품
+        pri = item.find("span", attrs = {"class":"nIAdxeTzhx"}).get_text() #가격
+        price = pri.replace(",","")
+        img = item.find("div", attrs = {"class":"_25CKxIKjAk"})['src']
+        siteName = "EARTHLING"
+        link_href = item.find("a")["href"]
+        link = "https://smartstore.naver.com"+link_href #판매링크
+        data = [main_category,sub_category,siteName,name, price, img, link, hash_tag]
+        writer.writerow(data)
+
+filecsv(items)
+f.close()
