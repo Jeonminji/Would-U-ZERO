e1f45824bc49f16065508473b9158bec1c2aaac6
hyunjungjeon <sksznddl574@gmail.com>
2021-07-08 09:01:43 +0000

    추천 상품 좌우 슬라이드 3초마다 자동 이동

--- WEB/js/recommend_item.js
+++ WEB/js/recommend_item.js
@@ -11,7 +11,7 @@ var slideWidth = 300;
 var slideMargin = 20;
 
 makeClone();
-
+// 무한 슬라이드로 만들기 위한 클론
 function makeClone(){
     for(var i = 0; i < slideCount; i++){
 
@@ -35,7 +35,7 @@ function makeClone(){
         slides.classList.add('animated');
     }, 100);
 }
-
+// width item 개수의 따라 변경
 function updateWidth(){
     var currentSlides = document.querySelectorAll('.rec_item');
     var newSlideCount = currentSlides.length;
@@ -43,7 +43,7 @@ function updateWidth(){
     var newWidth = (slideWidth + slideMargin)*newSlideCount+'px';
     slides.style.width = newWidth;
 }
-
+// 첫 화면상이 클론이 아닌 상품이 보여지게
 function setInitialPos(){
     var initialTranslateValue = -(slideWidth + slideMargin) * slideCount;
 
@@ -56,7 +56,7 @@ nextBtn.addEventListener('click',function(){
 prevBtn.addEventListener('click',function(){
     moveSlide(currentIdx-1);
 })
-
+// 버튼 클릭 시 move함수
 function moveSlide(num){
     slides.style.left = -num * (slideWidth + slideMargin) + 'px';
     currentIdx = num;
@@ -74,3 +74,31 @@ function moveSlide(num){
 
     }
 }
+
+// 아래 코드는 n초마다 한번씩 상품 자동 이동 && 마우스 hover시 n초 이동 멈춤 && 마우스 벗어나면 다시 이동
+var timer = undefined;
+
+function autoSlide(){
+    if(timer == undefined){
+        timer = setInterval(function(){
+            moveSlide(currentIdx + 1);
+        }, 3000);
+    }
+}
+
+autoSlide(); //3초마다 한 번씩 상품 하나 씩 이동
+
+function stopSlide(){
+    clearInterval(timer);
+    timer = undefined;
+}
+
+slides_outline.addEventListener('mouseenter', function(){
+    stopSlide();
+});
+
+slides_outline.addEventListener('mouseleave', function(){
+    autoSlide();
+});
+
+
