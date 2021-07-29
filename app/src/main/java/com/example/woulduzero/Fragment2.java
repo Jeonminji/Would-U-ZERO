fa0c03f5842c8affeff886bb1156ad4372be5268
ho-okim <khk821910@naver.com>
2021-07-29 16:32:38 +0900

    화면과 서랍 합치기

--- app/src/main/java/com/example/woulduzero/Fragment2.java
+++ app/src/main/java/com/example/woulduzero/Fragment2.java
@@ -1,13 +1,43 @@
 package com.example.woulduzero;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Fragment2 extends Fragment {
+    private RecyclerView recyclerView;
+    private CustomAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<OfflineShop> offlineShops, filteredList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
 
     public Fragment2() {
@@ -17,9 +47,124 @@ public class Fragment2 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+                             Bundle savedInstanceState) { // Inflate the layout for this fragment
+        View layout = inflater.inflate(R.layout.fragment_fragment2, container, false);
+
+//        layout.findViewById(R.id.btn_cafe).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(v.getContext(), "wow", Toast.LENGTH_LONG).show();
+//            }
+//        });
+
+
+        // 리사이클러뷰 초기화
+        recyclerView = (RecyclerView) layout.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
+        layoutManager = new GridLayoutManager(this.getContext(), 2);
+        recyclerView.setLayoutManager(layoutManager);
+        offlineShops = new ArrayList<>(); // OfflineShop 객체를 담을 어레이 리스트 (어댑터쪽으로)
+
+        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+
+        databaseReference = database.getReference("OfflineShop"); // DB 테이블 연결
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스 데이터베이스의 데이터 받아오는 곳
+                offlineShops.clear(); // 기존 배열리스트 방지 차원에서 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    OfflineShop offlineShop = snapshot.getValue(OfflineShop.class); // 오프라인 매장 객체에 데이터 담는다
+                    offlineShops.add(offlineShop); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                }
+                adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // DB 가져오는 과정에서 에러 발생 시
+                Log.e("Fragment2", String.valueOf(error.toException()));
+            }
+        });
+
+        adapter = new CustomAdapter(offlineShops, this.getContext());
+        recyclerView.setAdapter(adapter);
+
+
+//        Button btn_cafe = (Button) getActivity().findViewById(R.id.btn_cafe);
+//        btn_cafe.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                adapter = new CustomAdapter(searchFilter("카페"), Fragment2.super.getContext());
+//                recyclerView.setAdapter(adapter);
+//            }
+//        });
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btn_cafe: {
+                        searchFilter("카페");
+                        break;
+                    }
+                    case R.id.btn_refill: {
+                        Toast.makeText(v.getContext(), "refill", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.btn_zero: {
+                        Toast.makeText(v.getContext(), "zero", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                }
+            }
+        };
+
+        layout.findViewById(R.id.btn_cafe).setOnClickListener(clickListener);
+        layout.findViewById(R.id.btn_refill).setOnClickListener(clickListener);
+        layout.findViewById(R.id.btn_zero).setOnClickListener(clickListener);
+
+        return layout;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public void searchFilter(String searchText) {
+        filteredList.clear();
+
+        for(int i = 0; i < offlineShops.size(); i++) {
+            if(offlineShops.get(i).getStore_type().equals(searchText))
+                filteredList.add(offlineShops.get(i));
+        }
+
+        adapter.filterList(filteredList);
+    }
+
+//    public void onClick(View v) {
+//        switch (v.getId())
+//        {
+//            case R.id.btn_cafe:
+//            {
+//                adapter.notifyDataSetChanged();
+//                adapter = new CustomAdapter(searchFilter("카페"), this.getContext());
+//                recyclerView.setAdapter(adapter);
+//            }
+//            case R.id.btn_refill:
+//            {
+//                adapter = new CustomAdapter(searchFilter("리필 단일샵"), this.getContext());
+//                recyclerView.setAdapter(adapter);
+//                adapter.notifyDataSetChanged();
+//            }
+//            case R.id.btn_zero:
+//            {
+//                adapter = new CustomAdapter(searchFilter("제로웨이스트 샵 & 리필"), this.getContext());
+//                recyclerView.setAdapter(adapter);
+//                adapter.notifyDataSetChanged();
+//            }
+//        }
+//    }
 }
---
+++ app/src/main/java/com/example/woulduzero/OfflineshopInfo.java
@@ -0,0 +1,149 @@
+package com.example.woulduzero;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class OfflineshopInfo extends AppCompatActivity implements OnMapReadyCallback {
+    ImageView iv_img1;
+    ImageView iv_img2;
+    ImageView iv_img3;
+
+    int index = 0;
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offlineshop_info);
+
+        Bundle bundle = getIntent().getExtras();
+        OfflineShop of = bundle.getParcelable("offlineshop");
+
+        // URL 이미지를 이미지뷰에 연결
+        iv_img1 = findViewById(R.id.iv_1);
+        Glide.with(this).load(of.getImg1()).into(iv_img1);
+        iv_img2 = findViewById(R.id.iv_2);
+        Glide.with(this).load(of.getImg2()).into(iv_img2);
+        iv_img3 = findViewById(R.id.iv_3);
+        Glide.with(this).load(of.getImg3()).into(iv_img3);
+
+        TextView tv_strname = (TextView) findViewById(R.id.tv_strname);
+        tv_strname.setText("[ " + of.getStore_name() + " ]");
+
+        ImageButton ib_insta = (ImageButton) findViewById(R.id.ib_insta);
+        ib_insta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context c = v.getContext();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(of.getLink()));
+
+                c.startActivity(intent);
+            }
+        });
+
+        TextView tv_ophours = (TextView) findViewById(R.id.tv_ophours);
+        tv_ophours.setText(of.getOpening_hours());
+
+        TextView tv_ad = (TextView) findViewById(R.id.tv_ad);
+        tv_ad.setText(of.getAddress());
+
+        TextView tv_type = (TextView) findViewById(R.id.tv_type);
+        tv_type.setText(of.getStore_type());
+
+        TextView tv_pn = (TextView) findViewById(R.id.tv_pn);
+        tv_pn.setText(of.getStore_phone_number());
+        tv_pn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context c = v.getContext();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + of.getStore_phone_number().replace("-", "")));
+
+                c.startActivity(intent);
+            }
+        });
+
+        TextView tv_info = (TextView) findViewById(R.id.tv_info);
+        tv_info.setText(of.getOther_info().equals(" ") ? "없음" : of.getOther_info());
+
+
+        // 구글 맵 관련 코드
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void onArrowRightClicked(View v) {
+        index++;
+        index %= 3;
+
+        if(index == 0) {
+            iv_img1.setVisibility(View.VISIBLE);
+            iv_img2.setVisibility(View.INVISIBLE);
+            iv_img3.setVisibility(View.INVISIBLE);
+        } else if(index == 1) {
+            iv_img1.setVisibility(View.INVISIBLE);
+            iv_img2.setVisibility(View.VISIBLE);
+            iv_img3.setVisibility(View.INVISIBLE);
+        } else if(index == 2){
+            iv_img1.setVisibility(View.INVISIBLE);
+            iv_img2.setVisibility(View.INVISIBLE);
+            iv_img3.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void onArrowLeftClicked(View v) {
+        index += 2;
+        index %= 3;
+
+        if(index == 0) {
+            iv_img1.setVisibility(View.VISIBLE);
+            iv_img2.setVisibility(View.INVISIBLE);
+            iv_img3.setVisibility(View.INVISIBLE);
+        } else if(index == 1) {
+            iv_img1.setVisibility(View.INVISIBLE);
+            iv_img2.setVisibility(View.VISIBLE);
+            iv_img3.setVisibility(View.INVISIBLE);
+        } else if(index == 2){
+            iv_img1.setVisibility(View.INVISIBLE);
+            iv_img2.setVisibility(View.INVISIBLE);
+            iv_img3.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        Bundle bundle = getIntent().getExtras();
+        OfflineShop of = bundle.getParcelable("offlineshop");
+
+        LatLng location = new LatLng(of.getLat(), of.getLng()); // 해당 매장 위치에 마커 생성
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title(of.getStore_name());
+        markerOptions.snippet(of.getStore_type());
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+    }
+}
