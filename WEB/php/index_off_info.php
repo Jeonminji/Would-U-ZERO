92a963139cd9ee334433005ed00332a4b851c18f
hyunjungjeon <sksznddl574@gmail.com>
2021-07-29 01:32:36 +0000

    DB 이름 변경으로 코드 수정 / index페이지 online 상품 랜덤 도출

--- WEB/php/index_off_info.php
+++ WEB/php/index_off_info.php
@@ -1,19 +1,19 @@
 <?php
     header("Content-Type: application/json");
-    $link=mysqli_connect("localhost","admin","admin","test");
+    $link = mysqli_connect("localhost", "admin", "admin", "WZ");
     
     if(isset($_POST['store_name'])){
         $store_name =mysqli_real_escape_string($link, $_POST['store_name']);
        
     }else{
-        $query = "select store_name from off_test limit 1";
+        $query = "select store_name from offline_info limit 1";
         $result=mysqli_query($link, $query);
         while($row=mysqli_fetch_array($result)){
             $store_name = $row['store_name'];
         }
     }
 
-    $query = "SELECT * from off_test where store_name = '{$store_name}'";
+    $query = "SELECT * from offline_info where store_name = '{$store_name}'";
     $result=mysqli_query($link, $query);
     
     while($row=mysqli_fetch_array($result)){
--- WEB/php/update_click_count.php
+++ WEB/php/update_click_count.php
@@ -1,6 +1,6 @@
 <?php
     header("Content-Type: application/json");
-    $link=mysqli_connect("localhost","admin","admin","test");
+    $link = mysqli_connect("localhost", "admin", "admin", "WZ");
     
     if(isset($_GET['store_name'])){
         $store_name =mysqli_real_escape_string($link, $_GET['store_name']);
@@ -8,11 +8,11 @@
     }
     
 
-    $query = "UPDATE click_online SET click_count = click_count+1 WHERE product_name = '{$store_name}'";
+    $query = "UPDATE online_click SET click_count = click_count+1 WHERE product_name = '{$store_name}'";
     $result=mysqli_query($link, $query);
     
 
-    $query2 = "SELECT * FROM online_test WHERE online_test.name = '{$store_name}'";
+    $query2 = "SELECT * FROM online_info WHERE online_info.name = '{$store_name}'";
     $result2=mysqli_query($link, $query2);
     while($row=mysqli_fetch_array($result2)){
 
