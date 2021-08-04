57020e04d1f6ceecaf1664bb39e5debbc654ea3e
hyunjungjeon <sksznddl574@gmail.com>
2021-08-04 12:07:30 +0000

    index 페이지 온라인 상품 좌우 슬라이드, n초 슬라이드

--- WEB/php/index_online_item.php
+++ WEB/php/index_online_item.php
@@ -3,31 +3,55 @@
 
     $query = "SELECT * from online_info order by rand() limit 24";
     $online_items = '';
+    $online_items2 = '';
     $result = mysqli_query($link, $query);
 
-    while($row = mysqli_fetch_array($result)){
-
-        $price = substr_replace($row['price'],',',-6,0);
-        $online_items .= '<li class = "online_item">
-                            <div class = "item_box">
-                              <div class = "slide_img_box">
-                                <img src = "'.$row['img'].'" alt = "1">
-                                <div class = "overlay">
-                              
-                                <a onclick = "updateClick(\''.$row['name'].'\')" class = "buy_btn">Buy Now</a>
-                                  
-                                </div>
+    for($i = 0; $i<12; $i++){
+      $row = mysqli_fetch_array($result);
+      $online_items .= '<li class = "online_item">
+                          <div class = "item_box">
+                            <div class = "slide_img_box">
+                              <img src = "'.$row['img'].'" alt = "1">
+                              <div class = "overlay">
+                            
+                              <a onclick = "updateClick(\''.$row['name'].'\')" class = "buy_btn">Buy Now</a>
+                                
                               </div>
-                              <div class = "item_detail_box">
-                                <div class = "type">
-                                  <div class = "store_name">'.$row['siteName'].'</div>
-                                  <div class = "item_name">'.$row['name'].'</div>
-                                  <div class = "item_price">'.$price.'</div>
-                                </div>
+                            </div>
+                            <div class = "item_detail_box">
+                              <div class = "type">
+                                <div class = "store_name">'.$row['siteName'].'</div>
+                                <div class = "item_name">'.$row['name'].'</div>
+                                <div class = "item_price">'.$row['price'].'</div>
                               </div>
-                            </div>  
-                          </li>';
-        
+                            </div>
+                          </div>  
+                        </li>';
+     
+    }
+    
+    for($i = 0; $i<12; $i++){
+      $row = mysqli_fetch_array($result);
+      $online_items2 .= '<li class = "online_item">
+                          <div class = "item_box">
+                            <div class = "slide_img_box">
+                              <img src = "'.$row['img'].'" alt = "1">
+                              <div class = "overlay">
+                            
+                              <a onclick = "updateClick(\''.$row['name'].'\')" class = "buy_btn">Buy Now</a>
+                                
+                              </div>
+                            </div>
+                            <div class = "item_detail_box">
+                              <div class = "type">
+                                <div class = "store_name">'.$row['siteName'].'</div>
+                                <div class = "item_name">'.$row['name'].'</div>
+                                <div class = "item_price">'.$row['price'].'</div>
+                              </div>
+                            </div>
+                          </div>  
+                        </li>';
+                        
     }
 
 ?>
